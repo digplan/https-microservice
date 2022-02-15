51e9a1c874eafc9a65eaa979c069f1f783ae9ad8
CB <digplan@outlook.comm>
2022-02-14 21:59:57 -0600

    update

--- https-microservice.mjs
+++ https-microservice.mjs
@@ -1,6 +1,5 @@
-import { readFileSync, readdirSync } from 'node:fs';
+import { readFileSync, readdirSync, existsSync } from 'node:fs';
 import { Server } from 'node:https';
-import { env } from 'node:process';
 
 let dir = process.cwd()
 let dir_key = `${dir}/../keys/key.pem`
@@ -11,7 +10,7 @@ let dir_routes = `file://${dir}/routes`
 class HTTPSServer extends Server {
     middleware = []
     routes = {}
-    debug = process.env.debug
+    debug = process.env.debug || false
     constructor(key, cert) {
         super({
             key: readFileSync(dir_key),
@@ -19,7 +18,7 @@ class HTTPSServer extends Server {
         })
         this.on('request', (r, s) => {
             try {
-                if(debug) console.log(`${r.method} ${r.url}`)
+                if (this.debug) console.log(`${r.method} ${r.url}`)
                 let data = ''
                 r.on('data', (s) => {
                     data += s.toString()
@@ -31,10 +30,12 @@ class HTTPSServer extends Server {
                     try { data = JSON.parse(data) } catch (e) { }
                     r.server = this
                     this.middleware.some((f) => {
+                        if (this.debug) console.log(`middleware: ${f.name}`)
                         return f(r, s, data)
                     })
-                    if(r.url == '/') r.url = '/index.mjs'
+                    if (r.url == '/') r.url = '/index.mjs'
                     const route = this.routes[r.url]
+                    if (this.debug) console.log(`route: ${r.url}`)
                     if (!route)
                         return this.routes['/404'](r, s, data)
                     if (route.toString().startsWith('class')) {
@@ -54,19 +55,23 @@ class HTTPSServer extends Server {
     }
     async getMiddleware() {
         const mfiles = []
-        for (const model of readdirSync('./middleware'))
-            mfiles.push(model)
-        mfiles.sort((a, b) => a.localeCompare(b))
-        for (const model of mfiles) {
-            if (!model.endsWith('.mjs')) continue
-            let f = await import(`${dir_middleware}/${model}`)
-            this.middleware.unshift(f.default)
+        if (existsSync('./middleware')) {
+            for (const model of readdirSync('./middleware'))
+                mfiles.push(model)
+            mfiles.sort((a, b) => a.localeCompare(b))
+            for (const model of mfiles) {
+                if (!model.endsWith('.mjs')) continue
+                let f = await import(`${dir_middleware}/${model}`)
+                this.middleware.unshift(f.default)
+            }
         }
-        for (const model of readdirSync('./routes')) {
-            if (!model.endsWith('.mjs')) continue
-            const name = model.split('.')[0]
-            let f = (await import(`${dir_routes}/${model}`)).default
-            this.routes[`/${name}`] = f
+        if (existsSync('./routes')) {
+            for (const model of readdirSync('./routes')) {
+                if (!model.endsWith('.mjs')) continue
+                const name = model.split('.')[0]
+                let f = (await import(`${dir_routes}/${model}`)).default
+                this.routes[`/${name}`] = f
+            }
         }
     }
 }
